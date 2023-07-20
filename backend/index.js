164004d91ab5c327567ad573e46ec0bf973d2d1f
naveedrahid <naveedrashid685@gmail.com>
2023-07-20 20:30:14 +0500

    setup Project

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,9 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
 
-const DB = 'mongodb+srv://portfolioUser:Pixel99@cluster0.ijdzqei.mongodb.net/portfoliodb?retryWrites=true&w=majority';
 
-mongoose.connect(DB, {
-    // useNewUrlParser:true,
-    // useCreateIndex:true,
-    // useUnifiedTopology:true,
-    // useFindAndModify:false
+
+mongoose.connect(process.env.MONGO_DB , {
 }).then(() => {
     
     console.log('connected now');
---
+++ backend/model/userModel.js
@@ -0,0 +1,8 @@
+// import mongoose from 'mongoose';
+// const { Schema } = mongoose;
+
+// const productSchema = new mongoose.Schema({
+//     name: String,
+//     size: String
+// });
+// const Tank = mongoose.model('Tank', productSchema);
